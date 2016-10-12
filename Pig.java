c4ecb7aebeec4d54dd7a49d94f97d65bb7aee9fb
ersun1224 <ersun1224@gmail.com>
2016-10-12 14:35:23 -0700

    moving on

--- Pig.java
+++ Pig.java
@@ -1,11 +1,11 @@
 class Pig implements Animal {     
      private String myType;     
      private String mySound;      
-     public Cow(String type, String sound)     {         
+     public Pig(String type, String sound)     {         
          myType = type;         
          mySound = sound;     
      }     
-     public Cow()     {         
+     public Pig()     {         
          myType = "unknown";         
          mySound = "unknown";     
      }      
